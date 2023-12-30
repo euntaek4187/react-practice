bba2a8ba74ca0acafa7fa01b23ceb17bdf75a493
choieuntaek <choiet0225@gmail.com>
2023-12-30 11:37:47 +0900

    1230-practice

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,9 +7,11 @@ import { useState } from 'react';
 function App() {
 
   let post = "BLOG";
-  let [title, change] = useState(['남자 코트 추천', '바나나 추천', '사과 추천']);
+  let [title, setTitle] = useState(['여자 코트 추천', '바나나 추천', '사과 추천']);
+  let [titleStatus, setTitleStatue] = useState(0);
   let [good, setGood] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [input, setInput] = useState('');
 
   return (
     <div className="App">
@@ -17,44 +19,44 @@ function App() {
         <h3>{post}</h3>
       </div>
 
-      <button onClick={()=>{
-        let copy = [...title];
-        copy[0]="여자 코트 추천"
-        change(copy);
-      }}>변경</button>
+      <input onChange={(e)=>{setInput(e.target.value)}} type='text'></input>
+      <button onClick={()=>{setTitle([...title, input]); setGood([...good, 0])}}>작성</button>
 
       {
         title.map((a, i)=>{
           return (
             <div className='list' key={i}>
-            <h4 onClick={()=>{setModal(!modal)}}>{title[i]}
-              <span onClick={()=>{
+            <h4 onClick={()=>{setModal(!modal); setTitleStatue(i)}}>{title[i]}
+              <span onClick={(e)=>{
+                e.stopPropagation();
                 let copy = [...good];
                 copy[i] = copy[i] + 1;
                 setGood(copy)
                 }}> 따봉: {good[i]}</span>
             </h4>
             <p>2/25 published</p>
+            <button onClick={()=>{
+              let copy = [...title];
+              copy.splice(i, 1);
+              setTitle(copy);
+            }}>글 삭제</button>
           </div>
           )
         })
       }
 
       {
-          modal==true ? <Modal></Modal> : null
+          modal==true ? <Modal titleStatus={titleStatus} color={'skyblue'} title={title}></Modal> : null
       }
-      
-
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
-    <div className='modal'>
-        <h4>title</h4>
+    <div className='modal' style={{background:props.color}}>
+        <h4>{props.title[props.titleStatus]}</h4>
         <p>date</p>
-        <p>detail content</p>
     </div>
   )
 }
