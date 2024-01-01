b0633357660521150f5d1205b295bfedeb7a35d5
choieuntaek <choiet0225@gmail.com>
2024-01-01 19:56:27 +0900

    0101-practice-2

--- shop/src/routes/detail.js
+++ shop/src/routes/detail.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from 'styled-components'
 
 function Detail(props){
+
+    let [alert, setalert] = useState(true);
+    let [count] = useState(0);
+
+    useEffect(()=>{
+        setTimeout( ()=>{setalert(false)}, 2000);
+
+        return ()=>{
+            clearTimeout(alert);
+        }
+    }, [count])
+
 
     let {id} = useParams();
     let findId = props.shoes.find(function(x){
@@ -9,6 +23,13 @@ function Detail(props){
 
     return (
         <div className="container">
+
+            {
+                alert == true
+                ? <div>2초 이내 구매시 할인</div>
+                : null
+            }
+
         <div className="row">
             <div className="col-md-6">
             <img src={`https://codingapple1.github.io/shop/shoes${findId.id+1}.jpg`} width="100%" />
